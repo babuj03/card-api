18232317dcacece0202e7bd1266270a09f3edc51
Babu <babu.jayaraman-extern@nissan-nmuk.co.uk>
2021-04-05 00:03:59 +0100

    Initial code commit

---
+++ src/test/java/com/credit/card/app/repository/CreditCardRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.credit.card.app.repository;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.credit.card.app.entity.CardInfoEntity;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+
+
+@DataJpaTest
+public class CreditCardRepositoryTest {
+    @Autowired
+    private CreditCardRepository creditCardRepository;
+
+    @Test
+    public void testFindByCardNumber() {
+        CardInfoEntity cardInfoEntity = new CardInfoEntity();
+        cardInfoEntity.setUpdatedDateTime(LocalDateTime.now());
+        cardInfoEntity.setCreatedDateTime(LocalDateTime.now());
+        cardInfoEntity.setId(1L);
+        cardInfoEntity.setCreditlimit(10000.0);
+        cardInfoEntity.setCreatedBy("System");
+        cardInfoEntity.setName("Ganesh");
+        cardInfoEntity.setActive(false);
+        cardInfoEntity.setUpdatedBy("2020-03-01");
+        cardInfoEntity.setCardNumber("79927398713");
+        cardInfoEntity.setBalance(0.0);
+        this.creditCardRepository.save(cardInfoEntity);
+        assertNotNull(this.creditCardRepository.findByCardNumber("79927398713"));
+    }
+}
+
